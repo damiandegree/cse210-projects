852988a7792e719e2c5eeb6ccd76ea9988256c45
damiandegree <damian.degree96@gmail.com>
2026-05-10 23:37:34 -0300

    exercise 3 done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -9,25 +9,30 @@ class Program
          Random randomGenerator = new Random();
         int magicNumber = randomGenerator.Next(1, 101);
 
-        int guess = -1;
-        
-        while (guess != magicNumber)
-        {
-            Console.Write("What is your guess? ");
-            guess = int.Parse(Console.ReadLine());
+        string response="";
+        do{
+            
+            int guess = -1;
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
 
-            if (magicNumber > guess)
-            {
-                Console.WriteLine("Higher");
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
             }
-            else if (magicNumber < guess)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }
-        }
+            Console.WriteLine("Do you want to continue?: ");
+            response = Console.ReadLine();
+        }while(response=="yes");
     }
 }
