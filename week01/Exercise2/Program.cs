1a748e347e561acafdd08f2597cbcbf693079a86
damiandegree <damian.degree96@gmail.com>
2026-05-10 22:46:06 -0300

    exercise 2 done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,41 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade?: ");
+        string grade = Console.ReadLine();
+        int answer = int.Parse(grade);
+        string letter ="";
+        
+        if (answer >= 90)
+        {
+            letter="A";
+        }
+        else if (answer >= 80)
+        {
+            letter="B";
+        }
+        else if (answer >= 70)
+        {
+            letter="C";
+        }
+        else if(answer >= 60)
+        {
+            letter ="D";
+        }
+        else
+        {
+            letter="F";
+        }
+        Console.WriteLine($"Your grade is: {answer}, you have an {letter}.");
+
+        if (answer > 70)
+        {
+            Console.WriteLine("Well Done you passed.");
+        }
+        else
+        {
+            Console.WriteLine("Better luck the next time.");
+        }
+
     }
 }
