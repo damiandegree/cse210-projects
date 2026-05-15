b2b39b7a6832f04a6c7540d16e8ffe07beb0d6a6
damiandegree <damian.degree96@gmail.com>
2026-05-15 00:01:01 -0300

    activity done

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
-    }
+ Job job1 = new Job();
+        job1._jobTitle = "Logistic Operator";
+        job1._company = "La Virginia S.A";
+        job1._startYear = 2026;
+        job1._endYear = 2026;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Production Operator";
+        job2._company = "Cerro Negro S.A";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
+
+        Job job3 = new Job();
+        job3._jobTitle ="Production and Logistic Operator";
+        job3._company = "Coca Cola Andina S.A";
+        job3._startYear = 2018;
+        job3._endYear = 2021;
+
+        Resume myResume = new Resume();
+        myResume._name = "Matias Martinez";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume._jobs.Add(job3);
+
+        myResume.Display();    }
 }
